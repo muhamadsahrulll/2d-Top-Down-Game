f32b928f641d7279f6c15417a9bc87a5e68cd937
MuhamadSahrul <60306963+muhamadsahrulll@users.noreply.github.com>
2024-03-01 01:07:00 +0700

    add player

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 1f;
+
+    private PlayerControl playerControl;
+    private Vector2 movement;
+    private Rigidbody2D rb;
+
+    private Animator anim;
+    private SpriteRenderer sprite;
+
+
+    private void Awake()
+    {
+        playerControl = new PlayerControl();
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnEnable()
+    {
+        playerControl.Enable();
+    }
+
+    private void Update()
+    {
+        PlayerInput();
+    }
+
+    private void FixedUpdate()
+    {
+        AdjustPlayerFacingDirection();
+        Move();
+    }
+
+    private void PlayerInput()
+    {
+        movement = playerControl.Movement.Move.ReadValue<Vector2>();
+
+        anim.SetFloat("moveX", movement.x);
+        anim.SetFloat("moveY", movement.y);
+    }
+
+    private void Move()
+    {
+        rb.MovePosition(rb.position + movement * (moveSpeed * Time.fixedDeltaTime));
+    }
+
+    private void AdjustPlayerFacingDirection()
+    {
+        if (movement.x != 0f)
+        {
+            sprite.flipX = movement.x < 0f;
+        }
+        else if (movement.y != 0f)
+        {
+            sprite.flipX = false;
+        }
+    }
+}
