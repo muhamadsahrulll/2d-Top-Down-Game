4372c756bf8551b9aa1de20a267a571e7c45026a
MuhamadSahrul <60306963+muhamadsahrulll@users.noreply.github.com>
2024-03-01 10:42:28 +0700

    add enemy AI

---
+++ Assets/Script/EnemyPathfinding.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPathfinding : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 2f;
+
+    private Rigidbody2D rb;
+    private Vector2 moveDir;
+
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveDir * (moveSpeed * Time.fixedDeltaTime));
+
+        // Memastikan sprite menghadap ke arah yang benar
+        if (moveDir.x > 0)
+        {
+            // Menghadap ke kanan
+            FlipSprite(false);
+        }
+        else if (moveDir.x < 0)
+        {
+            // Menghadap ke kiri
+            FlipSprite(true);
+        }
+    }
+
+    private void FlipSprite(bool flipX)
+    {
+        // Mengatur sprite flipX
+        GetComponent<SpriteRenderer>().flipX = flipX;
+    }
+
+    public void MoveTo(Vector2 targetposition)
+    {
+        moveDir = (targetposition - (Vector2)transform.position).normalized;
+    }
+}
