f08a1c7d82748647856b38b6103bce3987c2ff32
MuhamadSahrul <60306963+muhamadsahrulll@users.noreply.github.com>
2024-05-25 10:51:33 +0700

    weapon shop level 1

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -25,9 +25,12 @@ public class GameManager : MonoBehaviour
     public GameObject tutor1;
     public GameObject selamat;
     public GameObject player;
-    
 
-    
+    public WeaponInfo weaponInfo1;
+    public WeaponInfo[] allWeaponInfos; // Tambahkan ini di inspector, isi dengan semua ScriptableObject senjata yang ada
+
+
+
 
 
 
@@ -49,11 +52,21 @@ public class GameManager : MonoBehaviour
         LoadGameData();
         UpdateUIText();
         tutor1.SetActive(true);
+        weaponInfo1.isPurchased = true;
         
+        // Memuat status pembelian senjata saat game dimulai
+        foreach (WeaponInfo weaponInfo in allWeaponInfos)
+        {
+            //ScoreManager.Instance.LoadWeaponPurchase(weaponInfo, 1);
+            ScoreManager.Instance.LoadWeaponPurchase(weaponInfo, 2);
+            ScoreManager.Instance.LoadWeaponPurchase(weaponInfo, 3);
+        }
+
     }
 
     private void Update()
     {
+        weaponInfo1.isPurchased = true;
         UpdateUIText();
         SaveGameData();
     }
@@ -122,6 +135,7 @@ public class GameManager : MonoBehaviour
     {
         RestartGame();
     }
+
 
 
 }
--- Assets/Script/UI/home.cs
+++ Assets/Script/UI/home.cs
@@ -2,9 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class home : MonoBehaviour
 {
+    
     public void KeluarGame()
     {
         Application.Quit();
